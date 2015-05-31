fe2f9cf8a8c4090560f76b29a03d7c4a34c63d67
Robert Nitsch <r.s.nitsch@gmail.com>
2015-05-31 10:56:45 +0200

    Add proper commandline arguments parsing to allow for custom parameters

--- RFA_demo.py
+++ RFA_demo.py
@@ -8,6 +8,7 @@ Source:     http://www.robertnitsch.de/de:informatik:rfa-traveling-salesman
 This package demonstrates the recursive fold algorithm (RFA) for metric
 traveling-salesman-problems. See the URL above for details (german).
 """
+import argparse
 import os
 import random
 import sys
@@ -16,25 +17,51 @@ import turtle
 from common import *
 from RFA import *
 
-def main(argv):
-    if len(argv) == 1:
-        selected = "random"
-    elif len(argv) == 2:
-        selected = argv[1]
-    else:
-        print("Usage: %s [random|tsplib]")
-        return 1
+def create_option_parser():
+    kwargs =  {
+              "description":
+              "Calculate and display routes for metric traveling-salesman-problems using the recursive-fold-algorithm."
+              }
 
-    if selected == "random":
-        main_random(seed=0)
-    elif selected == "tsplib":
-        main_tsplib(seed=0)
-    else:
-        print("Invalid selection: '%s'." % selected, file=sys.stderr)
+    parser = argparse.ArgumentParser(**kwargs)
+
+    # Add options to the OptionParser.
+    parser.add_argument("mode", type=str, action="store", help="Either 'demo' or 'benchmark'.")
+
+    DEFAULT_NUMBER_OF_NODES = 100
+    parser.add_argument("-n", "--nodes", type=int, action="store",
+                      dest="number_of_nodes", default=DEFAULT_NUMBER_OF_NODES,
+                      help=("Number of nodes for demo mode (default = %d)" % DEFAULT_NUMBER_OF_NODES))
+    
+    DEFAULT_SEED = 0
+    parser.add_argument("-s", "--seed", type=int, action="store",
+                      dest="seed", default=DEFAULT_SEED,
+                      help="Random number generator seed (default = %d)" % DEFAULT_SEED)
+
+    parser.add_argument("--no-rendering", action="store_true",
+                      dest="no_rendering", default=False,
+                      help="Do not render calculated routes")
+    
+    return parser
+
+def main(argv):
+    parser = create_option_parser()
+    args = parser.parse_args(args = argv[1:])
+
+    if not args.mode in ("demo", "benchmark"):
+        parser.error("Ungültiger Modus gewählt. Nur 'demo' oder 'benchmark' erlaubt.")
+    
+    if args.number_of_nodes <= 2:
+        parser.error("Anzahl der nodes muss größer-gleich 3 sein.")
+    
+    if args.mode == "demo":
+        main_random(args.number_of_nodes, args.seed, not args.no_rendering)
+    elif args.mode == "benchmark":
+        main_tsplib(args.seed, not args.no_rendering)
 
     return 0
 
-def main_random(seed=0):
+def main_random(number_of_nodes, seed=0, rendering_enabled=True):
     # KONFIGURATION:
 
     """
@@ -47,7 +74,7 @@ def main_random(seed=0):
 
     # ENDE DER KONFIGURATION.
     
-    nodes = generate_random_nodes(100, max_size=max_size)
+    nodes = generate_random_nodes(number_of_nodes, max_size=max_size)
 
     # Zur Reproduzierbarkeit.
     random.seed(seed)
@@ -64,9 +91,10 @@ def main_random(seed=0):
     print()
 
     # Darstellen der Route
-    paint_turtle(route)
+    if rendering_enabled:
+        paint_turtle(route, title="RFA demo with %d nodes and seed = %d (click to close)" % (number_of_nodes, seed))
 
-def main_tsplib(seed=0):
+def main_tsplib(seed=0, rendering_enabled=True):
     # KONFIGURATION:
 
     """
@@ -102,7 +130,8 @@ def main_tsplib(seed=0):
 
         rfa = RFABasic(nodes)
         route = rfa.run()
-        #print [str(i) for i in route]
+        if rendering_enabled:
+            paint_turtle(route, title="RFA route for TSPLIB instance '%s' with seed = %d (click to close)" % (tspi, seed))
 
         optimal_costs = tsplib_get_optimal_solution(tspi)
         total_costs   = route.get_total_costs()
@@ -115,7 +144,7 @@ def main_tsplib(seed=0):
                         'optimal_costs':    optimal_costs,
                         'factor':           factor})
 
-def paint_turtle(route, scale=1.5):
+def paint_turtle(route, scale=1.5, title="Route rendering (click to close)"):
     min_x = min([node.x for node in route])
     max_x = max([node.x for node in route])
     min_y = min([node.y for node in route])
@@ -137,7 +166,7 @@ def paint_turtle(route, scale=1.5):
     h = display_height + 2 * display_padding
     
     turtle.setup(width=w*scale, height=h*scale)
-    turtle.title("RFA demo (click to close)")
+    turtle.title(title)
 
     # For transforming data coordinates to turtle's screen coordinates.
     tc_x = lambda x: int((x - min_x) / data_width * display_width - display_width / 2.0) * scale
